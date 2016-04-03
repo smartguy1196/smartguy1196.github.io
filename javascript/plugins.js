78d967d1b8139a765b910d2e592ef247bac06e3a
smartguy1196 <smartguy1196@gmail.com>
2016-04-02 21:23:19 -0700

    Update plugins.js

--- javascript/plugins.js
+++ javascript/plugins.js
@@ -1,13 +1,17 @@
 //PopUpNoBlocker
 
 function popUpNoBlocker(href){
-  var el = document.createElement("a");
-  el.setAttribute('href',href);
-  if (el.fireEvent) {
-    el.fireEvent('onclick');
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent('click', true, false);
-    el.dispatchEvent(evObj);
+  if(/^#/g.test(href)){
+    
+  }else{
+    var el = document.createElement("a");
+    el.setAttribute('href',href);
+    if (el.fireEvent) {
+      el.fireEvent('onclick');
+    } else {
+      var evObj = document.createEvent('Events');
+      evObj.initEvent('click', true, false);
+      el.dispatchEvent(evObj);
+    }
   }
 }
