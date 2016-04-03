2a32ba81d4e859d19a4e532a15ec14856664e18b
smartguy1196 <smartguy1196@gmail.com>
2016-04-02 20:15:50 -0700

    Update implement-plugins.js

--- javascript/implement-plugins.js
+++ javascript/implement-plugins.js
@@ -1,7 +1,5 @@
 //PopUpNoBlocker
 (function() {
   var divs = document.querySelectorAll("[data-url]");
-  [].forEach.call(divs, function(div) {
-    div.onclick=popUpNoBlocker(div.getAttribute('data-url'));
-  });
+  alert(divs.length);
 });
