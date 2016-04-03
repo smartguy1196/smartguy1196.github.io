9618cbef71a8a90b5ee1e707a9edb2fe4f2dbbc5
smartguy1196 <smartguy1196@gmail.com>
2016-04-02 20:24:01 -0700

    Update implement-plugins.js

--- javascript/implement-plugins.js
+++ javascript/implement-plugins.js
@@ -2,10 +2,9 @@ window.onload = function(){
 
   //PopUpNoBlocker
   (function() {
-    alert("called");
     var divs = document.querySelectorAll("[data-url]");
     alert(divs.length);
-  });
+  })();
 
 
 
