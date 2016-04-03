b19527f61908b30e63f87de2ceb4202f103e87ce
smartguy1196 <smartguy1196@gmail.com>
2016-04-02 20:58:29 -0700

    Update implement-plugins.js

--- javascript/implement-plugins.js
+++ javascript/implement-plugins.js
@@ -5,7 +5,7 @@ window.onload = function(){
     var divs = document.querySelectorAll("[data-url]");
     for(i = 0; i < divs.length; i++)
       divs[i].onclick = function() {
-        window.popUpNoBlocker(divs[i].getAttribute("data-url");
+        window.popUpNoBlocker(divs[i].getAttribute("data-url"));
       };
   })();
 
