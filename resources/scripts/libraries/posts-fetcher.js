f17bccf880cebe3093b9179b32be80587b938b0d
Nicholas Jackson <smartguy1196@gmail.com>
2017-07-05 15:13:59 -0700

    Create posts-fetcher.js

---
+++ resources/scripts/libraries/posts-fetcher.js
@@ -0,0 +1,16 @@
+function feed( filters, handler ){
+  var init = {
+    "github" : {
+      "owner" : "smartguy1196",
+      "repo" : "smartguy1196.github.io",
+    },
+    "twitter" : {
+      "account" : "smartguy1196"
+    }
+  }
+  if( filters.source = "github" ){
+    
+  }else if( filters.source = "twitter" ){
+    
+  }
+}
