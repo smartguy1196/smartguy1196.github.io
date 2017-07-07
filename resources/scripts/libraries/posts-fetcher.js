50f7622ac1e79bbfc80b711619c27771ba9b9be8
Nicholas Jackson <smartguy1196@gmail.com>
2017-07-06 17:47:27 -0700

    Update posts-fetcher.js

--- resources/scripts/libraries/posts-fetcher.js
+++ resources/scripts/libraries/posts-fetcher.js
@@ -1,15 +1,16 @@
 function feed( filters, handler ){
   var init = {
-    "github" : {
-      "owner" : "smartguy1196",
-      "repo" : "smartguy1196.github.io",
-    },
-    "twitter" : {
-      "account" : "smartguy1196"
-    }
-  }
+        "github" : {
+          "owner" : "smartguy1196",
+          "repo" : "smartguy1196.github.io",
+        },
+        "twitter" : {
+          "account" : "smartguy1196"
+        }
+      },
+      api;
   if( filters.source = "github" ){
-    
+    api = "http://api.github.com/repos/" + init.owner + "/" + init.repo + "/issues"
   }else if( filters.source = "twitter" ){
     
   }
